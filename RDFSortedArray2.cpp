447d065629a3f01fdf0cd7a871ccbef6e918e969
esyrt <waterzhz@gmail.com>
2014-01-10 17:28:00 +0800

    create Search in Rotated Sorted Array

--- RDFSortedArray2.cpp
+++ RDFSortedArray2.cpp
@@ -7,20 +7,59 @@
  Given sorted array A = [1,1,1,2,2,3],
 
  Your function should return length = 5, and A is now [1,1,2,2,3].
- * $Id$ 
  * 
  **************************************************************************/
- 
+#include <stdio.h>
  
  
 class Solution {
 	public:
 		    int removeDuplicates(int A[], int n) {
-				        
-				    }
+				if(n==0)
+				{
+					return 0;
+				}
+				int i=0,j=1;
+				int flag=0;
+				while(j<n)
+				{
+					if(A[i]==A[j])
+					{
+						if(flag==0)
+						{
+							A[++i]=A[j++];
+							//A[i++]=A[j++]; wrong 2!
+							flag=1;
+						}
+						else
+						{
+							A[i]=A[j++];
+						}
+					}
+					else
+					{
+						flag=0;
+						A[++i]=A[j++];
+					}
+				}
+				return i+1;
+			}
 };
 
+int main()
+{
+    Solution so;
+	int a[6]={1,1,1,1,3,3};
+	int i, count;
+	count = so.removeDuplicates(a,6);
 
+	printf("count=%d\n",count);
+	for(i=0;i<count;i++)
+	{
+		printf("%d ",a[i]);
+	}
+	printf("\n");
+}
 
 
 
---
+++ SiRSortedArray.cpp
@@ -0,0 +1,45 @@
+/***************************************************************************
+ *Search in Rotated Sorted Array 
+ Total Accepted: 5531 Total Submissions: 19840
+ Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+ (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+ You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+ You may assume no duplicate exists in the array.
+ * 
+ **************************************************************************/
+ 
+ 
+ /*
+ * @date 2014/01/10 17:26:16
+ *  
+ **/
+class Solution {
+	public:
+		    int search(int A[], int n, int target) {
+				        
+				    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 noet: */
