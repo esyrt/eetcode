c8986adb70f21d1988c89d1c41658a26a8e59510
esyrt <waterzhz@gmail.com>
2014-01-20 17:37:30 +0800

    SiRSortedArray Accepted!

--- RDFSortedArray.cpp
+++ RDFSortedArray.cpp
@@ -54,7 +54,6 @@ int main()
 	}
 	printf("\n");
 return 0;
-
 }
 
 
--- SiRSortedArray.cpp
+++ SiRSortedArray.cpp
@@ -16,30 +16,118 @@
  * @date 2014/01/10 17:26:16
  *  
  **/
+#include <stdio.h>
 class Solution {
 	public:
 		    int search(int A[], int n, int target) {
-				        
+				int left=0;
+				int right=n-1;
+				int mid;
+				while(left<=right)
+				{
+					mid=(right-left)/2+left;
+					if(A[mid]==target)
+					{
+						return mid;
+					}
+					if(A[left]<A[mid])
+					{
+						if(target<A[mid]&&target>=A[left])
+						{
+							right = mid-1;
+						}
+						else
+						{
+							left = mid+1;
+						}
+					}
+					else
+					{
+						if(target>A[mid]&&target<=A[right])
+						{
+							left = mid+1;
+						}
+						else
+						{
+							right = mid-1;
+						}
+					}
+				}
+				return -1;
+
+				        /*
+						 * accepted
+						int prev=0;
+						int end=n-1;
+						int mid;
+						while(prev<=end)
+						{
+							mid=(end-prev)/2+prev;
+							if(A[prev]>A[mid])
+							{
+							    if(A[mid]<target && A[end]>target)
+								{
+									prev = mid+1;
+								}
+								else if((A[mid]>target && A[end]>target)||(A[mid]<target&&A[end]<target))
+								{
+									end = mid-1;
+								}
+								else if(A[mid] == target)
+								{
+									return mid;
+								}
+								else if(A[prev] == target)
+								{
+									return prev;
+								}
+								else if(A[end]==target)
+								{
+									return end;
+								}
+								else
+								{return -1;}
+							}
+							else
+							{
+								if(A[mid]>target && A[prev]<target)
+								{
+									end=mid-1;
+								}
+								else if((A[mid]>target && A[prev]>target)||(A[mid]<target && A[prev]<target))
+								{
+									prev = mid+1;
+								}
+								else if(A[mid]==target)
+								{
+									return mid;
+								}
+								else if(A[prev]==target)
+								{
+									return prev;
+								}
+								else if(A[end]==target)
+								{
+									return end;
+								}
+								else
+								{return -1;}
+							}
+						}
+                             return -1;
+							 */
 				    }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+int main()
+{
+	    Solution sol;
+		    int res;
+			    int A[2]={1,3};
+				    int arrsize = sizeof(A);
+					        res = sol.search(A,2,3);
+					            printf("%d\n",res);
+					            printf("\n");
+			                                    return 0;
+					                                    }           
 
 /* vim: set ts=4 sw=4 sts=4 tw=100 noet: */
