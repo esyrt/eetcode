d9065bcdef2d35fe873cf24ffd11609b53d87900
esyrt <waterzhz@gmail.com>
2014-01-22 15:27:55 +0800

    create prob LCS

---
+++ LCS.cpp
@@ -0,0 +1,49 @@
+/***************************************************************************
+ *Longest Consecutive Sequence  
+ Total Accepted: 5043 Total Submissions: 19061
+
+ Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+
+ For example,
+ Given [100, 4, 200, 1, 3, 2],
+ The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
+
+ Your algorithm should run in O(n) complexity.
+
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * @file LCS.cpp
+ * @author Hongzhe Zhu(zhuhongzhe@izptec.com)
+ * @date 2014/01/22 15:26:22
+ **/
+
+class Solution {
+	public:
+		    int longestConsecutive(vector<int> &num) {
+				        
+				    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 noet: */
